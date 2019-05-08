5782b1b2f0fa9f5a02f9f33dacad2356e4278c23
Ben Junya <bjunya@gmail.com>
2019-05-08 15:07:30 -0700

    Ready

--- src/Hourglass.js
+++ src/Hourglass.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { formatSeconds, formatTimerValues } from './utils';
 
 function Hourglass(props) {
-  const { showTimer, remainingSeconds } = props;
+  const { showTimer, remainingSeconds, totalSeconds } = props;
   const { hours, minutes, seconds } = formatSeconds(remainingSeconds);
   const formatted = formatTimerValues(hours, minutes, seconds);
   return (
     <div>
-      Hourglass
       {showTimer && `${formatted.hours}:${formatted.minutes}:${formatted.seconds}`}
+      <div className="hourglass">
+        <div className="hourglass-bar"
+          style={{ width: (
+            ((totalSeconds - remainingSeconds) / totalSeconds) * 100) + '%'
+          }}
+        />
+      </div>
     </div>
   );
 }
